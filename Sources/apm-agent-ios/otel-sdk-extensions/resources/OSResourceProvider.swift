6c0c880262b9434a2ae611b488ae6d74ef0bd8a8
Bryce Buchanan <bryce.buchanan@elastic.co>
2021-03-17 16:01:00 -0700

    refactored for testability

---
+++ Sources/apm-agent-ios/otel-sdk-extensions/resources/OSResourceProvider.swift
@@ -0,0 +1,40 @@
+// Copyright © 2021 Elasticsearch BV
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+import Foundation
+import UIKit
+import OpenTelemetryApi
+import OpenTelemetrySdk
+
+
+class OSResourceProvider : ResourceProvider {
+    let osDataSource : IOperatingSystemDataSource
+    
+    init(source: IOperatingSystemDataSource) {
+        osDataSource = source
+    }
+    
+    override var attributes: [String : AttributeValue] {
+        
+        var attributes = [String : AttributeValue]()
+        
+        attributes["os.type"] = AttributeValue.string(osDataSource.type)
+        attributes["os.description"] = AttributeValue.string(osDataSource.description)
+        
+        return attributes
+    }
+    
+    
+
+}
