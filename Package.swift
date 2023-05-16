4609990ea25ca1b10aebd06623efc584ddb4ccf0
Bryce Buchanan <bryce.buchanan@elastic.co>
2023-05-16 10:39:35 -0700

    added persistence to signals

--- Package.swift
+++ Package.swift
@@ -4,58 +4,64 @@
 import PackageDescription
 
 let package = Package(
-    name: "apm-agent-ios",
-    platforms: [
-        .iOS(.v11),
-        .macOS(.v10_13),
-        .tvOS(.v11),
-        .watchOS(.v4),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other package.
-        .library(name: "iOSAgent", type: .static, targets: ["iOSAgent"]),
-        .library(name: "MemorySampler", type: .static, targets: ["MemorySampler"]),
-        .library(name: "CPUSampler", type: .static, targets: ["CPUSampler"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
-        .package(url: "https://github.com/open-telemetry/opentelemetry-swift", exact:"1.4.1"),
-        .package(url:"https://github.com/elastic/TrueTime.swift.git", exact: "1.0.0"),
-        .package(url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.0.0")),
-    ],
-    targets: [
-        .target(name: "MemorySampler",
-                dependencies: [
-                    .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
-                    .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
-                ],
-                path: "Sources/Instrumentation/MemorySampler"),
-        .target(name: "CPUSampler",
-                dependencies: [
-                    .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
-                    .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
-                ],
-                path: "Sources/Instrumentation/CPUSampler"),
-        .target(
-            name: "iOSAgent",
-            dependencies: [
-                .product(name: "OpenTelemetryProtocolExporter", package: "opentelemetry-swift"),
-                .product(name: "URLSessionInstrumentation", package: "opentelemetry-swift"),
-                .product(name: "ResourceExtension", package: "opentelemetry-swift"),
-                .product(name: "Reachability", package: "Reachability.swift"),
-                .product(name: "TrueTime", package: "TrueTime.swift"),
-                .product(name: "CrashReporter", package: "plcrashreporter"),
-                "MemorySampler",
-                "CPUSampler",
-            ],
-            path: "Sources/apm-agent-ios"
-        ),
-        .testTarget(
-            name: "apm-agent-iosTests",
-            dependencies: ["iOSAgent"],
-            path: "Sources/Tests/apm-agent-iosTests"),
-        .testTarget(name: "memory-sampler-tests",
-                    dependencies: ["MemorySampler"],
-                    path: "Sources/Tests/memory-sampler-tests")
-    ]
+  name: "apm-agent-ios",
+  platforms: [
+    .iOS(.v11),
+    .macOS(.v10_13),
+    .tvOS(.v11),
+    .watchOS(.v4),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other package.
+    .library(name: "iOSAgent", type: .static, targets: ["iOSAgent"]),
+    .library(name: "MemorySampler", type: .static, targets: ["MemorySampler"]),
+    .library(name: "CPUSampler", type: .static, targets: ["CPUSampler"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
+    .package(
+      url: "https://github.com/open-telemetry/opentelemetry-swift", branch: "Persistent-LogRecords"),
+    .package(url: "https://github.com/elastic/TrueTime.swift.git", exact: "1.0.0"),
+    .package(
+      url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.0.0")),
+  ],
+  targets: [
+    .target(
+      name: "MemorySampler",
+      dependencies: [
+        .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
+        .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
+      ],
+      path: "Sources/Instrumentation/MemorySampler"),
+    .target(
+      name: "CPUSampler",
+      dependencies: [
+        .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
+        .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
+      ],
+      path: "Sources/Instrumentation/CPUSampler"),
+    .target(
+      name: "iOSAgent",
+      dependencies: [
+        .product(name: "OpenTelemetryProtocolExporter", package: "opentelemetry-swift"),
+        .product(name: "PersistenceExporter", package: "opentelemetry-swift"),
+        .product(name: "URLSessionInstrumentation", package: "opentelemetry-swift"),
+        .product(name: "ResourceExtension", package: "opentelemetry-swift"),
+        .product(name: "Reachability", package: "Reachability.swift"),
+        .product(name: "TrueTime", package: "TrueTime.swift"),
+        .product(name: "CrashReporter", package: "plcrashreporter"),
+        "MemorySampler",
+        "CPUSampler",
+      ],
+      path: "Sources/apm-agent-ios"
+    ),
+    .testTarget(
+      name: "apm-agent-iosTests",
+      dependencies: ["iOSAgent"],
+      path: "Sources/Tests/apm-agent-iosTests"),
+    .testTarget(
+      name: "memory-sampler-tests",
+      dependencies: ["MemorySampler"],
+      path: "Sources/Tests/memory-sampler-tests"),
+  ]
 )
--- Sources/apm-agent-ios/Instrumentation/CrashReporting/CrashManager.swift
+++ Sources/apm-agent-ios/Instrumentation/CrashReporting/CrashManager.swift
@@ -12,140 +12,152 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-import Foundation
-import OpenTelemetryApi
-import OpenTelemetrySdk
-import OpenTelemetryProtocolExporter
 import CrashReporter
-import Logging
-import os.log
+import Foundation
 import GRPC
+import Logging
 import NIO
-
+import OpenTelemetryApi
+import OpenTelemetryProtocolExporterCommon
+import OpenTelemetryProtocolExporterGrpc
+import OpenTelemetrySdk
+import os.log
 
 struct CrashManager {
-    static let CRASH_MANAGER_VERSION = "0.0.1"
-    static let LOG_LABEL = "Elastic-OTLP-Exporter"
-    static let lastResourceDefaultsKey : String = "elastic.last.resource"
-    let lastResource : Resource
-    let group : EventLoopGroup
-    let loggerProvider : LoggerProvider
-    init(resource: Resource, group: EventLoopGroup, agentConfiguration: AgentConfiguration) {
-        self.group = group
-        // if something went wrong with the lastResource in the user defaults, fallback of the current resource data.
-        var tempResource = resource
-        
-        let otlpConfiguration = OtlpConfiguration(timeout: OtlpConfiguration.DefaultTimeoutInterval, headers: OpenTelemetryHelper.generateExporterHeaders(agentConfiguration.auth))
+  static let crashManagerVersion = "0.0.1"
+  static let logLabel = "Elastic-OTLP-Exporter"
+  static let lastResourceDefaultsKey: String = "elastic.last.resource"
+  let lastResource: Resource
+  let group: EventLoopGroup
+  let loggerProvider: LoggerProvider
+  init(resource: Resource, group: EventLoopGroup, agentConfiguration: AgentConfiguration) {
+    self.group = group
+    // if something went wrong with the lastResource in the user defaults, fallback of the current resource data.
+    var tempResource = resource
 
-        
-        if let lastResourceJson  = UserDefaults.standard.data(forKey: Self.lastResourceDefaultsKey) {
-            do {
-                let decoder = JSONDecoder()
-                tempResource = try decoder.decode(Resource.self, from: lastResourceJson)
-            } catch {
-                os_log("[Elastic][CrashManager] initialization: unable to load last Resource from user defaults.")
-            }
-        }
-        lastResource = tempResource
-        loggerProvider = LoggerProviderBuilder()
-            .with(resource: lastResource)
-            .with(processors: [SimpleLogRecordProcessor(logRecordExporter: OtlpLogExporter(channel: OpenTelemetryHelper.getChannel(with: agentConfiguration, group: group),
-                                                                                           config: otlpConfiguration,
-                                                                                           logger: Logger(label:Self.LOG_LABEL), envVarHeaders: OpenTelemetryHelper.generateExporterHeaders(agentConfiguration.auth)))])
-            .build()
-        do {
-            let encoder = JSONEncoder()
-            let data = try encoder.encode(resource)
-            UserDefaults.standard.set(data, forKey:Self.lastResourceDefaultsKey)
-        } catch {
-            os_log("[Elastic][CrashManager] initialization: unable to save current Resource from user defaults.")
+    let otlpConfiguration = OtlpConfiguration(
+      timeout: OtlpConfiguration.DefaultTimeoutInterval,
+      headers: OpenTelemetryHelper.generateExporterHeaders(agentConfiguration.auth))
 
-        }
+    if let lastResourceJson = UserDefaults.standard.data(forKey: Self.lastResourceDefaultsKey) {
+      do {
+        let decoder = JSONDecoder()
+        tempResource = try decoder.decode(Resource.self, from: lastResourceJson)
+      } catch {
+        os_log(
+          "[Elastic][CrashManager] initialization: unable to load last Resource from user defaults."
+        )
+      }
     }
-    
-    public func initializeCrashReporter() {
-        // It is strongly recommended that local symbolication only be enabled for non-release builds.
-        // Use [] for release versions.
-        let config = PLCrashReporterConfig(signalHandlerType: .mach, symbolicationStrategy: [])
-        guard let crashReporter = PLCrashReporter(configuration: config) else {
-          print("Could not create an instance of PLCrashReporter")
-          return
-        }
+    lastResource = tempResource
+    loggerProvider = LoggerProviderBuilder()
+      .with(resource: lastResource)
+      .with(processors: [
+        SimpleLogRecordProcessor(
+          logRecordExporter: OtlpLogExporter(
+            channel: OpenTelemetryHelper.getChannel(with: agentConfiguration, group: group),
+            config: otlpConfiguration,
+            logger: Logger(label: Self.logLabel),
+            envVarHeaders: OpenTelemetryHelper.generateExporterHeaders(agentConfiguration.auth)))
+      ])
+      .build()
+    do {
+      let encoder = JSONEncoder()
+      let data = try encoder.encode(resource)
+      UserDefaults.standard.set(data, forKey: Self.lastResourceDefaultsKey)
+    } catch {
+      os_log(
+        "[Elastic][CrashManager] initialization: unable to save current Resource from user defaults."
+      )
 
-        // Enable the Crash Reporter.
-        do {
-            if (!isDebuggerAttached()) {
-                try crashReporter.enableAndReturnError()
-            }
-        } catch let error {
-          print("Warning: Could not enable crash reporter: \(error)")
-        }
-        
-        // Try loading the crash report.
-        if crashReporter.hasPendingCrashReport() {
-          do {
-            let data = try crashReporter.loadPendingCrashReportDataAndReturnError()
-              let lp = loggerProvider.loggerBuilder(instrumentationScopeName: "PLCrashReporter")
-                  .setInstrumentationVersion(Self.CRASH_MANAGER_VERSION)
-                  .setEventDomain("device")
-                  .build()
-                  
-              
-            // Retrieving crash reporter data.
-            let report = try PLCrashReport(data: data)
+    }
+  }
 
-            // We could send the report from here, but we'll just print out some debugging info instead.
-            if let text = PLCrashReportTextFormatter.stringValue(for: report, with: PLCrashReportTextFormatiOS) {
-              print(text)
-                // notes : branching code needed for signal vs mach vs nsexception for event generation
-                //
-                var attributes = [
-                    "exception.type": AttributeValue.string(report.signalInfo.name),
-                    "exception.stacktrace": AttributeValue.string(text)
-                ]
-                if let code = report.signalInfo.code {
-                    attributes["exception.message"] = AttributeValue.string("\(code) at \(report.signalInfo.address)")
-                }
-                
-                lp.eventBuilder(name: "crash")
-                    .setSeverity(.fatal)
-                    .setObservedTimestamp(report.systemInfo.timestamp)
-                    .setAttributes(attributes)
-                    .emit()
-                
-            } else {
-              print("CrashReporter: can't convert report to text")
-            }
-          } catch let error {
-            print("CrashReporter failed to load and parse with error: \(error)")
+  public func initializeCrashReporter() {
+    // It is strongly recommended that local symbolication only be enabled for non-release builds.
+    // Use [] for release versions.
+    let config = PLCrashReporterConfig(signalHandlerType: .mach, symbolicationStrategy: [])
+    guard let crashReporter = PLCrashReporter(configuration: config) else {
+      print("Could not create an instance of PLCrashReporter")
+      return
+    }
+
+    // Enable the Crash Reporter.
+    do {
+      if !isDebuggerAttached() {
+        try crashReporter.enableAndReturnError()
+      }
+    } catch let error {
+      print("Warning: Could not enable crash reporter: \(error)")
+    }
+
+    // Try loading the crash report.
+    if crashReporter.hasPendingCrashReport() {
+      do {
+        let data = try crashReporter.loadPendingCrashReportDataAndReturnError()
+        let lp = loggerProvider.loggerBuilder(instrumentationScopeName: "PLCrashReporter")
+          .setInstrumentationVersion(Self.crashManagerVersion)
+          .setEventDomain("device")
+          .build()
+
+        // Retrieving crash reporter data.
+        let report = try PLCrashReport(data: data)
+
+        // We could send the report from here, but we'll just print out some debugging info instead.
+        if let text = PLCrashReportTextFormatter.stringValue(
+          for: report, with: PLCrashReportTextFormatiOS)
+        {
+          print(text)
+          // notes : branching code needed for signal vs mach vs nsexception for event generation
+          //
+          var attributes = [
+            "exception.type": AttributeValue.string(report.signalInfo.name),
+            "exception.stacktrace": AttributeValue.string(text),
+          ]
+          if let code = report.signalInfo.code {
+            attributes["exception.message"] = AttributeValue.string(
+              "\(code) at \(report.signalInfo.address)")
           }
-              
-        }
 
-        // Purge the report.
-        crashReporter.purgePendingCrashReport()
-    }
-    
-    private func isDebuggerAttached() -> Bool {
-        var info = kinfo_proc()
-        let info_size = UnsafeMutablePointer<Int>.allocate(capacity: 1)
-        info_size[0] = MemoryLayout<kinfo_proc>.size
-        let name  = UnsafeMutablePointer<Int32>.allocate(capacity: 4)
-        
-        name[0] = CTL_KERN
-        name[1] = KERN_PROC
-        name[2] = KERN_PROC_PID
-        name[3] = getpid()
-        
-        if (sysctl(name, 4, &info, info_size, nil, 0) == -1) {
-            print("sysctl() failed: \(String(describing: strerror(errno)))")
-            return false
+          lp.eventBuilder(name: "crash")
+            .setSeverity(.fatal)
+            .setObservedTimestamp(report.systemInfo.timestamp)
+            .setAttributes(attributes)
+            .emit()
+
+        } else {
+          print("CrashReporter: can't convert report to text")
         }
-        
-        if ((info.kp_proc.p_flag & P_TRACED) != 0) {
-            return true
-        }
-    
-        return false
+      } catch let error {
+        print("CrashReporter failed to load and parse with error: \(error)")
+      }
+
     }
+
+    // Purge the report.
+    crashReporter.purgePendingCrashReport()
+  }
+
+  private func isDebuggerAttached() -> Bool {
+    var info = kinfo_proc()
+    let infoSize = UnsafeMutablePointer<Int>.allocate(capacity: 1)
+    infoSize[0] = MemoryLayout<kinfo_proc>.size
+    let name = UnsafeMutablePointer<Int32>.allocate(capacity: 4)
+
+    name[0] = CTL_KERN
+    name[1] = KERN_PROC
+    name[2] = KERN_PROC_PID
+    name[3] = getpid()
+
+    if sysctl(name, 4, &info, infoSize, nil, 0) == -1 {
+      print("sysctl() failed: \(String(describing: strerror(errno)))")
+      return false
+    }
+
+    if (info.kp_proc.p_flag & P_TRACED) != 0 {
+      return true
+    }
+
+    return false
+  }
 }
