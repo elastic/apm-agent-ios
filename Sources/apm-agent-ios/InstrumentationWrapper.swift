c6c9173c1c72b4bdc9afd5aed081ebd7eb1798d4
Bryce Buchanan <75274611+bryce-b@users.noreply.github.com>
2023-10-30 10:03:01 -0700

    Http errors (#183)
    * remove 'throws'
    
    * added http error as network span event
    
    * reverted otel-swift version

--- Sources/apm-agent-ios/InstrumentationWrapper.swift
+++ Sources/apm-agent-ios/InstrumentationWrapper.swift
@@ -21,38 +21,38 @@ import OpenTelemetryApi
 
 
 class InstrumentationWrapper {
-        
-    var appMetrics : Any?
-
-    #if os(iOS)
-        var vcInstrumentation: ViewControllerInstrumentation?
-        var netstatInjector: NetworkStatusInjector?
-    var applicationLifecycleInstrumentation : ApplicationLifecycleInstrumentation?
-    #endif
     
-
+    var appMetrics : Any?
+    
+#if os(iOS)
+    var vcInstrumentation: ViewControllerInstrumentation?
+    var netstatInjector: NetworkStatusInjector?
+    var applicationLifecycleInstrumentation : ApplicationLifecycleInstrumentation?
+#endif
+    
+    
     var urlSessionInstrumentation: URLSessionInstrumentation?
     let config : AgentConfigManager
     
     init(config: AgentConfigManager) {
         self.config = config
         
-        #if os(iOS)
-            if config.instrumentation.enableLifecycleEvents {
-                applicationLifecycleInstrumentation = ApplicationLifecycleInstrumentation()
+#if os(iOS)
+        if config.instrumentation.enableLifecycleEvents {
+            applicationLifecycleInstrumentation = ApplicationLifecycleInstrumentation()
+        }
+        do {
+            if self.config.instrumentation.enableViewControllerInstrumentation {
+                vcInstrumentation = try ViewControllerInstrumentation()
             }
-            do {
-                if self.config.instrumentation.enableViewControllerInstrumentation {
-                    vcInstrumentation = try ViewControllerInstrumentation()
-                }
-            } catch {
-                print("failed to initalize view controller instrumentation: \(error)")
-            }
-        #endif // os(iOS)
+        } catch {
+            print("failed to initalize view controller instrumentation: \(error)")
+        }
+#endif // os(iOS)
     }
     
     func initalize() {
-        #if os(iOS)
+#if os(iOS)
         if #available(iOS 13.0, *) {
             if config.instrumentation.enableSystemMetrics {
                 _ = MemorySampler()
@@ -61,53 +61,64 @@ class InstrumentationWrapper {
             if config.instrumentation.enableAppMetricInstrumentation {
                 appMetrics = AppMetrics()
                 if let metrics = appMetrics as? AppMetrics {
-                   metrics.receiveReports()
-               }
+                    metrics.receiveReports()
+                }
             }
         }
-        #endif
+#endif
         if config.instrumentation.enableURLSessionInstrumentation {
             initializeNetworkInstrumentation()
         }
-        #if os(iOS)
-            vcInstrumentation?.swizzle()
-        #endif // os(iOS)
+#if os(iOS)
+        vcInstrumentation?.swizzle()
+#endif // os(iOS)
     }
     
     private func initializeNetworkInstrumentation() {
-        #if os(iOS)
-            do {
-                let netstats = try NetworkStatus()
-                netstatInjector = NetworkStatusInjector(netstat: netstats)
-            } catch {
-                print("failed to initialize network connection status \(error)")
-            }
-        #endif
-
+#if os(iOS)
+        do {
+            let netstats =  try NetworkStatus()
+            netstatInjector = NetworkStatusInjector(netstat: netstats)
+        } catch {
+            print("failed to initialize network connection status \(error)")
+        }
+#endif
+        
         let config = URLSessionInstrumentationConfiguration(shouldRecordPayload:nil,
                                                             shouldInstrument: nil,
                                                             nameSpan: { request in
-                                                                if let host = request.url?.host, let method = request.httpMethod {
-                                                                    return "\(method) \(host)"
-                                                                }
-                                                                return nil
-                                                            },
+            if let host = request.url?.host, let method = request.httpMethod {
+                return "\(method) \(host)"
+            }
+            return nil
+        },
                                                             shouldInjectTracingHeaders: nil,
                                                             createdRequest: { _, span in
-                                                                #if os(iOS)
-                                                                    if let injector = self.netstatInjector {
-                                                                        injector.inject(span: span)
-                                                                    }
-                                                                #endif
-                                                            },
-                                                            receivedResponse: nil,
+#if os(iOS)
+            if let injector = self.netstatInjector {
+                injector.inject(span: span)
+            }
+#endif
+        },
+                                                            receivedResponse: { response, _, span in
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode >= 400 && httpResponse.statusCode <= 599 {
+                    span.addEvent(name : SemanticAttributes.exception.rawValue,
+                                  attributes: [SemanticAttributes.exceptionType.rawValue: AttributeValue.string("\(httpResponse.statusCode)"),
+                                               SemanticAttributes.exceptionEscaped.rawValue: AttributeValue.bool(false),
+                                               SemanticAttributes.exceptionMessage.rawValue: AttributeValue.string(HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode)),
+                                              ])
+                }
+            }
+            
+        },
                                                             receivedError: { error, _, _, span in
-                                                                span.addEvent(name: SemanticAttributes.exception.rawValue,
-                                                                              attributes: [SemanticAttributes.exceptionType.rawValue: AttributeValue.string(String(describing: type(of: error))),
-                                                                                           SemanticAttributes.exceptionEscaped.rawValue: AttributeValue.bool(false),
-                                                                                           SemanticAttributes.exceptionMessage.rawValue: AttributeValue.string(error.localizedDescription)])
-                                                            })
-
+            span.addEvent(name: SemanticAttributes.exception.rawValue,
+                          attributes: [SemanticAttributes.exceptionType.rawValue: AttributeValue.string(String(describing: type(of: error))),
+                                       SemanticAttributes.exceptionEscaped.rawValue: AttributeValue.bool(false),
+                                       SemanticAttributes.exceptionMessage.rawValue: AttributeValue.string(error.localizedDescription)])
+        })
+        
         urlSessionInstrumentation = URLSessionInstrumentation(configuration: config)
     }
 }
