38eb672938c9f3f8776869436b7be01814e4634a
Bryce Buchanan <bryce.buchanan@elastic.co>
2024-01-02 09:27:30 -0800

    updated otel-swift to version 1.9.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
     .package(
-      url: "https://github.com/open-telemetry/opentelemetry-swift", exact: "1.8.0"),
+      url: "https://github.com/open-telemetry/opentelemetry-swift", exact: "1.9.0"),
     .package(url: "https://github.com/elastic/TrueTime.swift.git", exact: "1.0.0"),
     .package(
       url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.0.0")),
