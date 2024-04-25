8fa4ba37bf68d2581c056204ac0a9fde8570df68
Pavel Aliferka <pavel_aliferka@epam.com>
2024-04-25 14:51:30 +0300

    - update the latest Reachability v5.2.2 to support Privacy manifest policy

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .library(name: "CPUSampler", type: .static, targets: ["CPUSampler"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
+    .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.2.2"),
     .package(
       url: "https://github.com/open-telemetry/opentelemetry-swift", exact: "1.9.1"),
     .package(url: "https://github.com/MobileNativeFoundation/Kronos.git", .upToNextMajor(from: "4.2.2")),
