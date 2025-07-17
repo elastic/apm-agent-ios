35a2d40d89b750c1d7699b8f06e0710584ef8182
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-07-17 09:42:33 -0700

    added OpampHttpRequestService
    added state management structure
    added Request & Response structure
    added OpampClient & Callbacks

---
+++ Sources/apm-agent-ios/Configuration/Opamp/client/OpampClientBuilder.swift
@@ -0,0 +1,164 @@
+//
+//  Copyright © 2025  Elasticsearch BV
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+import Foundation
+import OpenTelemetryApi
+import OpenTelemetrySdk
+
+public struct OpampClientBuilder {
+  private let remoteConfigStatusState = OpampRemoteConfigStatusState()
+  private let sequenceNumberState = OpampSequenceNumberState()
+  private let agentDescriptionState = OpampAgentDescriptionState()
+  private let capabilitiesState = OpampCapabilitiesState()
+  private let instanceUidState = OpampInstanceUidState()
+  private let effectiveConfigState = OpampEffectiveConfigState()
+
+  public func build(requestService: some RequestService) -> OpampClient {
+    let clientState = OpampClientState(
+      remoteConfigStatusState: remoteConfigStatusState,
+      sequenceNumberState: sequenceNumberState,
+      agentDescriptionState: agentDescriptionState,
+      capabilitiesState: capabilitiesState,
+      instanceUidState: instanceUidState,
+      effectiveConfigState: effectiveConfigState
+    )
+    return OpampClientImpl(
+      requestService: requestService,
+      clientState: clientState
+    )
+  }
+
+  public mutating func setInstsanceUid(_ instanceUid : UUID) -> Self {
+    instanceUidState.value = instanceUid
+    return self
+  }
+
+  public mutating func setServiceName(_ serviceName: String) -> Self {
+    return addIdentifyingAttribute(
+      key: ResourceAttributes.serviceName.rawValue,
+      value: .string(serviceName))
+  }
+
+  public mutating func setServiceNamespace(_ serviceNamespace: String) -> Self {
+    return addIdentifyingAttribute(
+      key: ResourceAttributes.serviceNamespace.rawValue,
+      value: .string(serviceNamespace)
+    )
+  }
+
+  public mutating func setServiceVersion(_ serviceVersion: String) -> Self {
+    return addIdentifyingAttribute(
+      key: ResourceAttributes.serviceVersion.rawValue,
+      value: .string(serviceVersion))
+  }
+
+  public mutating func setServiceEnvironment(_ serviceEnvironment: String) -> Self {
+   return addIdentifyingAttribute(
+      key: "deployment.environment.name" ,
+      value: .string(serviceEnvironment)
+    )
+  }
+
+  public mutating func enableRemoteConfig() -> Self {
+
+    return self
+  }
+
+  public mutating func addIdentifyingAttribute(key: String, value: AttributeValue) -> Self {
+    agentDescriptionState.value.identifyingAttributes
+      .append(OpampClientBuilder.createKeyValue(key:key, attributeValue:value))
+    return self
+  }
+  private static func createKeyValue(key: String, attributeValue: AttributeValue) -> Opamp_Proto_KeyValue {
+    var keyValue = Opamp_Proto_KeyValue()
+    keyValue.key = key
+    switch attributeValue {
+    case let .string(value):
+      keyValue.value.stringValue = value
+    case let .bool(value):
+      keyValue.value.boolValue = value
+    case let .int(value):
+      keyValue.value.intValue = Int64(value)
+    case let .double(value):
+      keyValue.value.doubleValue = value
+
+    case let .set(value):
+      keyValue.value.kvlistValue.values = value.labels.map {
+        return createKeyValue(key: $0, attributeValue: $1)
+      }
+    case let .array(value):
+      keyValue.value.arrayValue.values = value.values.map {
+        return toProtoAnyValue(attributeValue: $0)
+      }
+    case let .stringArray(value):
+      keyValue.value.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .string($0))
+      }
+    case let .boolArray(value):
+      keyValue.value.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .bool($0))
+      }
+    case let .doubleArray(value):
+      keyValue.value.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .double($0))
+      }
+    case let .intArray(value):
+      keyValue.value.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .int($0))
+      }
+    }
+    return keyValue
+  }
+
+  private static func toProtoAnyValue(attributeValue: AttributeValue) -> Opamp_Proto_AnyValue {
+    var anyValue = Opamp_Proto_AnyValue()
+    switch attributeValue {
+    case let .string(value):
+      anyValue.stringValue = value
+    case let .bool(value):
+      anyValue.boolValue = value
+    case let .int(value):
+      anyValue.intValue = Int64(value)
+    case let .double(value):
+      anyValue.doubleValue = value
+    case let .set(value):
+      anyValue.kvlistValue.values = value.labels.map {
+        return createKeyValue(key: $0, attributeValue: $1)
+      }
+    case let .array(value):
+      anyValue.arrayValue.values = value.values.map {
+        return toProtoAnyValue(attributeValue: $0)
+      }
+    case let .stringArray(value):
+      anyValue.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .string($0))
+      }
+    case let .boolArray(value):
+      anyValue.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .bool($0))
+      }
+    case let .intArray(value):
+      anyValue.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .int($0))
+      }
+    case let .doubleArray(value):
+      anyValue.arrayValue.values = value.map {
+        return toProtoAnyValue(attributeValue: .double($0))
+      }
+    }
+    return anyValue
+  }
+  internal init() {}
+}
---
+++ Sources/apm-agent-ios/Configuration/Opamp/client/OpampClientCallback.swift
@@ -0,0 +1,55 @@
+//
+//  Copyright © 2025  Elasticsearch BV
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+import Foundation
+
+public protocol OpampClientCallback {
+  /// Called when the connection to the Sever is successfully established. May be called after
+  /// `OpampClient.start(callback _:)` is called & every time a connection is established to
+  ///  the Server. For HTTP clients this is called for any request if the resonse status is *OK*. '
+  ///  - Parameter client: the relevant `OpampClient`
+  func onConnect(client: OpampClient);
+
+  /// Called when the conenction to the Server cannot be established. May bre called after
+  /// `OpampClient.start(callback _:)` is called and tries to connect to the Server.
+  ///  May also be called if the connection is lost and reconnection atempt fails.
+  /// - Parameters:
+  ///  - client: the relevant `OpampClient`
+  ///  - error: The connection error
+  ///  - retryAfter: The `TimeInterval` afterwhich a retry will be attempted.
+  func onConnectFailed(client: OpampClient, error: Error, retryAfter: TimeInterval);
+
+  /// Called when the Server reports an error in response to some previously sent request. Useful for
+  /// logging purposes. The Agent should not attempt to process the error by reconnecting or retrying
+  /// previous operations. The client handles the error response `UNAVAILABLE` case internally by
+  /// performing retries as necessary.
+  /// - Parameters:
+  ///  - client: the relevant `OpampClient`
+  ///  - error: The response error
+  ///  - retryAfter: the `TimeInterval` afterwhich the retry is attempted.
+  func onErrorResponse(client: OpampClient, error: Error, retryAfter: TimeInterval);
+
+  /// Called when the Agent receives a message that needs to be processed. See `OpampMessage`
+  /// definition for the data that may be available for processing. During `OnMessage` execution the
+  /// `OpampClient` functions that change the status of the client may be called, e.g.: if RemoteConfig
+  /// is processed then `OpampClient.setRemoteConfigStatus()` should be called to reflect the
+  /// processing result. These functions may also be called after `onMessage` returns. This is advisable
+  /// if processing can take a long time. In that case returning quickly is preferable to to avoid blocking
+  /// the `OpampClient`.
+  /// - Parameters:
+  ///  - client: The relevant `OpampClient`
+  ///  - message: The server response data that needs processing.
+  func onMessage(client: OpampClient, message: OpampMessage);
+}
