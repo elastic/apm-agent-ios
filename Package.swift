0fa5960ad7e84095009564c5741ea367f6206357
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-12-08 14:46:08 -0800

    updated test to use latest semantic conventions

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.2.4"),
     .package(
-      url: "https://github.com/open-telemetry/opentelemetry-swift", from: "2.1.0"),
+      url: "https://github.com/open-telemetry/opentelemetry-swift", from: "2.2.1"),
     .package(
-      url: "https://github.com/open-telemetry/opentelemetry-swift-core", from: "2.1.1"),
+      url: "https://github.com/open-telemetry/opentelemetry-swift-core", from: "2.3.0"),
     .package(url: "https://github.com/MobileNativeFoundation/Kronos.git", .upToNextMajor(from: "4.2.2")),
     .package(
       url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.12.0")),
